122d91007285dc921df7713320b3b68a6ebc52d5
rathina12 <rathinamoorthy.c2023csbs@sece.ac.in>
2026-06-17 07:07:09 +0530

    FirstLastSame

---
+++ FirstLastSame.java
@@ -0,0 +1,7 @@
+public class FirstLastSame {
+    public static void main(String[] args) {
+        int[] arr = {50, -20, 0, 30, 40, 60, 10};
+
+        System.out.println(arr[0] == arr[arr.length - 1]);
+    }
+}
